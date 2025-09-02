fc86058d6d8c83cd74c1b5639396aae542e345d9
ritonun <73615644+ritonun@users.noreply.github.com>
2025-09-02 13:31:01 +0200

    button to scan for devices

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, render_template, jsonify
-from .smart import get_smart_status
+from flask import Blueprint, render_template, jsonify, request
+from .smart import get_smart_status, get_devices
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
+@main.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        if "scan" in request.form:
+            get_devices()
+
     return render_template("index.html")
--- flaskr/smart.py
+++ flaskr/smart.py
@@ -1,5 +1,10 @@
 import subprocess
 
+
+def get_devices() -> list:
+    return []
+
+
 def get_smart_status():
     print("wip")
     return
